204f227c31edced57dcd3e6ab883b6bb7890d1a2
Adarsh Kumar <117112030+adarsh-k27r@users.noreply.github.com>
2023-09-23 22:59:12 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/taleDB";
+// const mongoURI = "mongodb://127.0.0.1:27017/taleDB";
+
+const mongoURI = "mongodb+srv://admin-adarsh:mongoDB-pswd@cluster0.3cwun6d.mongodb.net/taleDB";
 
 const connectMongo = ()=>{
     mongoose.connect(mongoURI);
